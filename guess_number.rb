0f2b0090f47cd29f177146a9c89c99d0ffffc6f2
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-10-02 12:29:54 -0700

    first ruby

---
+++ guess_number.rb
@@ -0,0 +1,8 @@
+def guess_number (guess)
+    number = 25
+    puts "Guess was too high" if guess > number
+    puts "Guess was too low" if guess < number
+    puts "You got it!" if guess==number
+end
+
+guess_number (25)
