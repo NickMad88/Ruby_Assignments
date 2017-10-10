03a1c01a971fe232f2db86494cbcb1c11fa2d285
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-10-10 12:50:21 -0700

    dojodash2

--- DojoDashboard/app/controllers/dojos_controller.rb
+++ DojoDashboard/app/controllers/dojos_controller.rb
@@ -2,4 +2,12 @@ class DojosController < ApplicationController
     def index
         @dojos = Dojo.all
     end
+
+    def new
+    end
+
+    def create
+        Dojo.create(branch: params[:branch], street: params[:street], city: params[:city], state: params[:state])
+        redirect_to '/dojos'
+    end
 end
