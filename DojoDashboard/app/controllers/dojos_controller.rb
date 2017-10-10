201524719b4072a63632f3e5f9b7b071b810b821
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-10-10 13:36:08 -0700

    dojodash

--- DojoDashboard/app/controllers/dojos_controller.rb
+++ DojoDashboard/app/controllers/dojos_controller.rb
@@ -10,4 +10,30 @@ class DojosController < ApplicationController
         Dojo.create(branch: params[:branch], street: params[:street], city: params[:city], state: params[:state])
         redirect_to '/dojos'
     end
+
+    def show
+        @dojos = Dojo.find(params[:id])
+    end
+
+    def edit
+        @dojos = Dojo.find(params[:id])
+    end
+
+    def update
+        dojos = Dojo.find(params[:id])
+        dojos.branch = params[:branch]
+        dojos.street = params[:street]
+        dojos.city = params[:city]
+        dojos.state = params[:state]
+        dojos.save
+
+        redirect_to '/dojos'
+                
+    end
+
+    def destroy
+        dojos = Dojo.find(params[:id])
+        dojos.destroy
+        redirect_to '/dojos'
+    end
 end
