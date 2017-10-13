b259a4f9196dd07f499fc6eb04e5208ddd7de7d4
Nick Madani <nickmadani@Nicks-MacBook-Pro.local>
2017-10-13 12:51:31 -0700

    project

--- project/project.rb
+++ project/project.rb
@@ -1,22 +1,12 @@
 class Project
-attr_accessor :name, :description, :owner
-attr_reader :tasks
-def initialize name, description, owner
-  @name = name
-  @description = description
-  @owner = owner
-  @tasks = []
-end
+  attr_accessor :name, :description, :owner
 
-def elevator_pitch
-  "#{@name}, #{@description}"
-end
+  def initialize name, desc
+    @name = name
+    @description = desc
+  end
 
-def print_tasks
-  @tasks.each { |elem| puts elem }
-end
-
-def add_tasks task
-  @tasks << task
-end
+  def elevator_pitch
+    "#{@name}, #{@description}"
+  end
 end
